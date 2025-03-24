3ce10fa4bca43680de428aa283672a855b8cd319
Alex Poputa <poputa.alex@gmail.com>
2025-03-24 21:51:27 +0200

    finished the boards and columns management functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import ThemeToggle from "./components/themeToggle";
 import Welcome from "./pages/WelcomePage";
 import { useDispatch, useSelector } from "react-redux";
-import { selectBoards, selectUserData } from "./redux/selectors";
-import { fetchBoards, loginUser } from "./services/reduxServices";
+import { selectColumns, selectUserData } from "./redux/selectors";
+import { fetchColumns, loginUser } from "./services/reduxServices";
 
 const App = () => {
   const userData = useSelector(selectUserData);
-  const boards = useSelector(selectBoards);
+  const boards = useSelector(selectColumns);
   const dispatch = useDispatch();
   console.log(userData);
   console.log(boards);
 
   const handleClick = () => {
-     dispatch(loginUser({email:'alex@email.com', password:'123123123'}))
+    dispatch(loginUser({ email: "alex@email.com", password: "123123123" }));
+    dispatch(fetchColumns("67e03dd656c61f514590c2c8"));
     // dispatch(createBoard({ title: "board 1232", background: "http://localhost:5000/images/star-sky.jpg" }));
-    dispatch(fetchBoards());
   };
   return (
     <>
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,7 +3,6 @@ import apiClient from "../utils/apiClient";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-
 // Registers a new user
 // This function should be called when the user clicks the register button on the register page
 export const registerUser = async (userData) => {
@@ -26,3 +25,70 @@ export const createBoard = async (boardData) => {
   }
 };
 
+// Updates a board
+// This function should be called when the user clicks the update button in the edit board modal
+export const updateBoard = async (boardId, boardData) => {
+  try {
+    const response = await apiClient.patch(`/boards/${boardId}`, boardData);
+    return response.status;
+  } catch (error) {
+    return error.response.message;
+  }
+};
+
+// Deletes a board
+// This function should be called when the user clicks the confirm delete button
+export const deleteBoard = async (boardId) => {
+  try {
+    const response = await apiClient.delete(`/boards/${boardId}`);
+    return response.status;
+  } catch (error) {
+    return error.response.message;
+  }
+};
+
+// Creates a new column
+// This function should be called when the user clicks the create button in the create column modal
+// columnData should contain the boardId and the title of the column
+export const createColumn = async (columnData) => {
+  try {
+    const response = await apiClient.post("/columns", columnData);
+    return response.status;
+  } catch (error) {
+    return error.response.message;
+  }
+};
+
+// Deletes a column
+// This function should be called when the user clicks the confirm delete button in the delete column modal
+export const deleteColumn = async (columnId) => {
+  try {
+    const response = await apiClient.delete(`/columns/${columnId}`);
+    return response.status;
+  } catch (error) {
+    return error.response.message;
+  }
+};
+
+// Fetches all cards for a specific column
+// This function should be called when the user clicks on a board and the columns are fetched
+export const fetchCards = async (columnId) => {
+  try {
+    const response = await apiClient.get(`/cards/${columnId}`);
+    return response.data;
+  } catch (error) {
+    return error.response.message;
+  }
+};
+
+// Creates a new card
+// This function should be called when the user clicks the add button in the add card modal
+// cardData should contain the columnId, title, description, label, and deadline of the card
+export const createCard = async (cardData) => {
+  try {
+    const response = await apiClient.post("/cards", cardData);
+    return response.status;
+  } catch (error) {
+    return error.response.message;
+  }
+};
